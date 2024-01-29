22f1b4b7183d2640eff148d3c460bf5a32a39a23
David Nguyen <h250694@gmail.com>
2024-01-29 09:56:06 +0700

    Update INSIGHT_BOT_USERNAME and INSIGHT_BOT_EMAIL

--- src/helper/git/commit-git.js
+++ src/helper/git/commit-git.js
@@ -1,7 +1,7 @@
 const git = require('../../core/git');
 let commitGit = function () {
-    let INSIGHT_BOT_USERNAME = 'github-actions[bot]';
-    let INSIGHT_BOT_EMAIL = '41898282+github-actions[bot]@users.noreply.github.com';
+    let INSIGHT_BOT_USERNAME = process.env.INSIGHT_BOT_USERNAME || 'eplus-bot';
+    let INSIGHT_BOT_EMAIL = process.env.INSIGHT_BOT_EMAIL || 'bot@eplus.dev';
     let commit = async function (message) {
         console.log(`Git Commit "${message}"`)
         try {
