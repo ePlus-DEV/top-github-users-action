64b34e65136987cf124d774f6e516c2540abe30c
David Nguyen <h250694@gmail.com>
2024-01-31 13:30:27 +0700

    Fix broken image URLs in headerComponent.js

--- dist/index.js
+++ dist/index.js
@@ -14630,7 +14630,7 @@ let headerComponent = function () {
             markdown = markdown + `slug: /\n`;
             markdown = markdown + `---\n\n`;
             markdown = markdown + `# Top GitHub Users By Country `;
-            markdown = markdown + `[<img alt="Image of insights" src="https://github.com/ePlus-DEV/view-counter/blob/main/graph/749591754/small/week.png" height="24"/>](https://github.com/ePlus-DEV/view-counter/blob/master/readme/749591754/week.md)\n`
+            markdown = markdown + `[<img alt="Image of insights" src="https://github.com/ePlus-DEV/view-counter/raw/main/graph/749591754/small/week.png" height="24"/>](https://github.com/ePlus-DEV/view-counter/blob/master/readme/749591754/week.md)\n`
             markdown = markdown + `[![Top GitHub Users](https://github.com/ePlus-DEV/top-github-users/actions/workflows/top.yml/badge.svg)](https://github.com/ePlus-DEV/top-github-users/actions/workflows/top.yml) `;
             markdown = markdown + `[![Image of insights](https://github.com/ePlus-DEV/view-counter/blob/main/svg/749591754/badge.svg)](https://github.com/ePlus-DEV/view-counter/blob/master/readme/749591754/week.md)\n\n`;
         } else {
@@ -14639,7 +14639,7 @@ let headerComponent = function () {
             markdown = markdown + `description: Top GitHub Users By ${pageTitle} in ${country} \n`;
             markdown = markdown + `---\n\n`;
             markdown = markdown + `# Top GitHub Users By ${pageTitle} in ${country} `;
-            markdown = markdown + `[<img alt="Image of insights" src="https://github.com/ePlus-DEV/view-counter/blob/main/graph/749591754/small/week.png" height="24"/>](https://github.com/ePlus-DEV/view-counter/blob/master/readme/749591754/week.md)\n`
+            markdown = markdown + `[<img alt="Image of insights" src="https://github.com/ePlus-DEV/view-counter/raw/main/graph/749591754/small/week.png" height="24"/>](https://github.com/ePlus-DEV/view-counter/blob/master/readme/749591754/week.md)\n`
             markdown = markdown + `[![Top GitHub Users](https://github.com/ePlus-DEV/top-github-users/actions/workflows/top.yml/badge.svg)](https://github.com/ePlus-DEV/top-github-users/actions/workflows/top.yml) `;
             markdown = markdown + `[![Image of insights](https://github.com/ePlus-DEV/view-counter/blob/main/svg/749591754/badge.svg)](https://github.com/ePlus-DEV/view-counter/blob/master/readme/749591754/week.md)\n\n`;
         }
